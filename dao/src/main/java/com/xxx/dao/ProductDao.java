01b1a229f168202cbfd1f4bcac21da43e5058d49
bcl@163.com <bcl@163.com>
2018-12-01 15:22:28 +0800

    ProductDao接口的编写

--- dao/src/main/java/com/xxx/dao/ProductDao.java
+++ dao/src/main/java/com/xxx/dao/ProductDao.java
@@ -1,4 +1,15 @@
 package com.xxx.dao;
 
+import com.xxx.domain.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+
+    /**
+     * 查询所有的方法
+     */
+    List<Product> findAllProduct();
 }
+
+
